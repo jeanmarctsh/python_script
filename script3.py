04902737eda52dddba02984d471d3dc5b4cd3af7
jeanmarctshi <jeanmarctshimbombo@gmail.com>
2026-03-14 17:16:25 -0700

    Supprime script2.py et met à jour script3.py

---
+++ script3.py
@@ -0,0 +1,27 @@
+## Utilisation de fabric pour se loginecter en SSH vers les machines clientes
+## Exécution de différentes commandes systèmes
+
+from fabric import loginection as loginnect, task
+from server_list import servers 
+
+## Déclarer les di_fférentes commandes à exécuter simultanément sur les machines clientes
+
+@task 
+
+def check_remote_sservers(cheeck):
+    for server in servers:
+        
+        try:
+            login= loginnect(
+                host=server["host"],
+                user=server["user"],
+                port=server["port"]
+            )
+
+            result = login.run("which node_exporter", warn=True, hide=True)
+
+            print(f"[OK] {server['host']} -> {result.stdout.strip()}")
+
+        except Exception as e:
+            print(f"[ERROR] Impossible de se loginecter à {server['host']}")
+            print(e)
