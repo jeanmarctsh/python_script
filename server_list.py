d279a47f7b32ebfd03ce061d1582f2f43ec86cb7
jeanmarctshi <jeanmarctshimbombo@gmail.com>
2026-03-14 17:08:40 -0700

    information de différentes machines clientes

---
+++ server_list.py
@@ -0,0 +1,28 @@
+
+## INFORRMATION RELATIVE AUX DIFFERENTES MACHINES CLIENTES
+## PROJET N COURS DE DEVELOPPEMENT
+
+servers = [
+
+    {
+        "host" : "192.168.9.109",
+        "user" : "awx",
+        "port" : "22"
+
+    },
+
+     {
+        "host" : "192.168.9.115",
+        "user" : "ansible",
+        "port" : "22"
+
+    },
+
+     {
+        "host" : "192.168.9.120",
+        "user" : "marc",
+        "port" : "22"
+
+    },
+
+]
