a3314214b91ea01bb425c91e513e48e2c723c72c
jeanmarctshi <jeanmarctshimbombo@gmail.com>
2026-03-15 15:48:53 -0700

    Exécution séquentielle de différentes commandes

--- script3.py
+++ script3.py
@@ -1,23 +1,55 @@
-## Utilisation de fabric pour se loginecter en SSH vers les machines clientes
+## Utilisation de fabric pour se connecter en SSH vers les machines clientes
 ## Exécution de différentes commandes systèmes
 
-from fabric import loginection as loginnect, task
+from fabric import Connection as connnect, task
 from server_list import servers 
 
-## Déclarer les di_fférentes commandes à exécuter simultanément sur les machines clientes
+## Renseignement de différentes commandes à exécuter simultanément au niveau des machines clientes
 
-@task 
+from fabric import Connection, task
+from server_list import servers
 
-def check_remote_sservers(cheeck):
+remote_commands = [
+    "ip a | grep ens",
+    "sudo apt update",
+    "sudo apt-cache policy prometheus-node-exporter",
+    "sudo apt install node_exporter-1.10.2.linux-amd64 -y",
+    "sudo systemctl start node_exporter",
+    "sudo systemctl status node_exporter"
+]
+
+@task
+def login_run(c):
+
     for server in servers:
-        
-        try:
-            login= loginnect(
-                host=server["host"],
-                user=server["user"],
-                port=server["port"]
-            )
 
-            result = login.run("which node_exporter", warn=True, hide=True)
+        host = server.get("host")
+        user = server.get("user")
+        port = server.get("port", 22)
 
-          ## en cours de développement
+        if host and user:
+
+            try:
+                print(f"\nConnexion vers {host} avec l'utilisateur {user}")
+
+                con = Connection (
+                    host=host,
+                    user=user,
+                    port=port,
+                    connect_kwargs={ "key_filename":  ####### ssh private key-path 
+                             }
+                 )
+
+                for cmd in remote_commands:
+                    print(f"Exécution de : {cmd}")
+                    if cmd.startswith("sudo"):
+                        result = con.sudo(cmd.replace("sudo ", ""), hide=False)
+                    else:
+                        result = con.run(cmd, hide=False)
+                    print(result.stdout)
+
+            except Exception as e:
+                print(f"Impossible de se connecter à {host}: {e}")
+
+        else:
+            print(f"Veuillez renseigner correctement les informations pour : {server}")
